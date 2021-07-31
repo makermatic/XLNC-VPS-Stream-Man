73deaf6480648ab11d3d60dd5794d5c15d555c86
makermatic <44180512+makermatic@users.noreply.github.com>
2021-07-30 20:06:20 -0400

    fixes

--- bundles/xlnc_bundle/graphics/Caster_Desk.js
+++ bundles/xlnc_bundle/graphics/Caster_Desk.js
@@ -57,37 +57,36 @@ function changeTeam1Image() {
     }
 
     // F8
-    else if (team1Name.innerHTML.includes("F8") || team1Name.innerHTML.includes("f8")) {
+    if (team1Name.innerHTML.includes("F8") || team1Name.innerHTML.includes("f8")) {
         t1Image.src = './teamLogos/F8_white.png'
     }
 
     // Lotus
-    else if (team1Name.innerHTML.includes("Lotus") || team1Name.innerHTML.includes("lotus")) {
+    if (team1Name.innerHTML.includes("Lotus") || team1Name.innerHTML.includes("lotus")) {
         t1Image.src = './teamLogos/Lotus_white.png'
     }
 
     // Nightless Kaqzi
-    else if (team1Name.innerHTML.includes("Nightless") || team1Name.innerHTML.includes("nightless")) {
+    if (team1Name.innerHTML.includes("Nightless") || team1Name.innerHTML.includes("nightless")) {
         t1Image.src = './teamLogos/Nightless Kaqzi_white.png'
     }
 
     // Pepe Gang
-    else if (team1Name.innerHTML.includes("Pepe") || team1Name.innerHTML.includes("pepe")) {
+    if (team1Name.innerHTML.includes("Pepe") || team1Name.innerHTML.includes("pepe")) {
         t1Image.src = './teamLogos/Pepe Gang.png'
     }
 
+<<<<<<< HEAD
     // Team Shiiiiish
     else if (team1Name.innerHTML.includes("Sh") || team1Name.innerHTML.includes("sh")) {
         t1Image.src = './teamLogos/defaultLogo.png'
     }
 
+=======
+>>>>>>> parent of c70d3e2 (Default logo override when team tag isn't present)
     // Valkyrie
-    else if (team1Name.innerHTML.includes("Valkyrie") || team1Name.innerHTML.includes("valkyrie")) {
+    if (team1Name.innerHTML.includes("Valkyrie") || team1Name.innerHTML.includes("valkyrie")) {
         t1Image.src = './teamLogos/Valkyrie_white.png'
-
-        // Default Image
-    } else {
-        t1Image.src = './teamLogos/defaultLogo.png'
     }
 
     console.log('Team 1 image: ', t1Image.src)
@@ -103,37 +102,36 @@ function changeTeam2Image() {
     }
 
     // F8
-    else if (team2Name.innerHTML.includes("F8") || team2Name.innerHTML.includes("f8")) {
+    if (team2Name.innerHTML.includes("F8") || team2Name.innerHTML.includes("f8")) {
         t2Image.src = './teamLogos/F8.png'
     }
 
     // Lotus
-    else if (team2Name.innerHTML.includes("Lotus") || team2Name.innerHTML.includes("lotus")) {
+    if (team2Name.innerHTML.includes("Lotus") || team2Name.innerHTML.includes("lotus")) {
         t2Image.src = './teamLogos/Lotus.png'
     }
 
     // Nightless Kaqzi
-    else if (team2Name.innerHTML.includes("Nightless") || team2Name.innerHTML.includes("night")) {
+    if (team2Name.innerHTML.includes("Nightless") || team2Name.innerHTML.includes("night")) {
         t2Image.src = './teamLogos/Nightless Kaqzi.png'
     }
 
     // Pepe Gang
-    else if (team2Name.innerHTML.includes("Pepe") || team2Name.innerHTML.includes("pepe")) {
+    if (team2Name.innerHTML.includes("Pepe") || team2Name.innerHTML.includes("pepe")) {
         t2Image.src = './teamLogos/Pepe Gang.png'
     }
 
+<<<<<<< HEAD
     // Team Shiiiiish
     else if (team1Name.innerHTML.includes("Sh") || team1Name.innerHTML.includes("sh")) {
         t2Image.src = './teamLogos/defaultLogo.png'
     }
 
+=======
+>>>>>>> parent of c70d3e2 (Default logo override when team tag isn't present)
     // Valkyrie
-    else if (team2Name.innerHTML.includes("Valkyrie") || team2Name.innerHTML.includes("valkyrie")) {
+    if (team2Name.innerHTML.includes("Valkyrie") || team2Name.innerHTML.includes("valkyrie")) {
         t2Image.src = './teamLogos/Valkyrie.png'
-
-        // Default Image
-    } else {
-        t2Image.src = './teamLogos/defaultLogo.png'
     }
 
 
