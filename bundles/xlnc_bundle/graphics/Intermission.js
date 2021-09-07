2770050b4837a114ac4c6b1aae49bede1025af81
makermatic <44180512+makermatic@users.noreply.github.com>
2021-09-07 13:28:55 -0400

    Revert "Added maze layout"
    This reverts commit b49fcdf3df50b304fb85e9ea9487d32739e2a22a.

--- bundles/xlnc_bundle/graphics/Intermission.js
+++ bundles/xlnc_bundle/graphics/Intermission.js
@@ -1,20 +1,6 @@
 const team1Name = document.getElementById('team1Name');
 const team2Name = document.getElementById('team2Name');
 
-const team1Rep = nodecg.Replicant('team1');
-const team2Rep = nodecg.Replicant('team2');
-
-const team1El = document.getElementById('team1');
-const team2El = document.getElementById('team2');
-
-team1Rep.on('change', (newVal) => {
-    team1El.innerHTML = newVal;
-})
-
-team2Rep.on('change', (newVal) => {
-    team2El.innerHTML = newVal;
-})
-
 nodecg.listenFor('updateTeams', (data) => {
     team1Name.innerHTML = data.team1Name
     team2Name.innerHTML = data.team2Name
